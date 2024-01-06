b5bba09293ecb81eaceacd3949d636840ca26a69
Vincent Huang iMac <yfr.huang@hotmail.com>
2024-01-07 01:21:45 +0800

    :sparkles: Remove unused .js.map file
    The main.dd7496bd.js.map file was removed because it these are automatically generated files and are not needed in the repository. The file was previously checked into version control but is no longer necessary and has been removed to declutter the repository.

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,46 @@ import React from 'react';
 import ImageAvatar from './assets/avatar.jpg';
 import { FaFacebook, FaGithub, FaGoogle, FaLinkedin, FaMicrophone, FaRegNewspaper, FaTelegram, FaTwitter, FaYoutube } from 'react-icons/fa6';
 
-function App() {
+export default function App() {
   return (
-    <article className={`my-8 flex flex-col items-center gap-4`}>
-      <img src={ImageAvatar} alt="Avatar" className={`h-64 w-64 rounded-full border-4 border-[#e89619] shadow-2xl`} />
-      <h1 className={`text-xl font-bold text-white`}>Jian-Min (Vincent) Huang</h1>
-      <p className={`text-white`}>👨‍💻 Coder｜ 🎤 Speaker｜ 👨‍🏫 Mentor｜ 🚀 Leader</p>
-      <section className={`mt-4 flex max-w-[280px] flex-wrap justify-center gap-8 text-white`}>
+    <article className={`my-8 flex flex-col items-center gap-8`}>
+      <img src={ImageAvatar} alt="Avatar" className={`h-64 w-64 rounded-full border-4 border-[#e89619]`}/>
+      <div className={`flex flex-col items-center gap-4 font-mono text-white`}>
+        <h1 className={`text-xl font-bold`}>Jian-Min (Vincent) Huang</h1>
+        <p className={`text-lg font-semibold`}>
+          <span className={`text-[#e89619]`}>$_purefunc</span>, Co-founder
+        </p>
+        <p className={`text-base font-medium`}>👨‍💻Coder, 🎤Speaker, 👨‍🏫Mentor, 🚀Leader</p>
+      </div>
+      <section className={`flex max-w-[280px] flex-wrap justify-center gap-8 text-white`}>
         <a href={`https://docs.google.com/document/d/1w3OKshhacU1vv2I6tAx-GSqNue8Nj_jm-_zHPkTzgrw`}>
-          <FaRegNewspaper size={72} />
+          <FaRegNewspaper size={72}/>
         </a>
         <a href="https://www.facebook.com/yfr.huang/">
-          <FaFacebook size={72} />
+          <FaFacebook size={72}/>
         </a>
         <a href="https://github.com/Jian-Min-Huang/tech-note/wiki">
-          <FaMicrophone size={72} />
+          <FaMicrophone size={72}/>
         </a>
         <a href="https://developers.google.com/community/experts/directory/profile/profile-jian-min-huang">
-          <FaGoogle size={72} />
+          <FaGoogle size={72}/>
         </a>
         <a href="https://github.com/Jian-Min-Huang">
-          <FaGithub size={72} />
+          <FaGithub size={72}/>
         </a>
         <a href="https://www.linkedin.com/in/jian-min-huang">
-          <FaLinkedin size={72} />
+          <FaLinkedin size={72}/>
         </a>
         <a href="https://twitter.com/JianMin_V_Huang">
-          <FaTwitter size={72} />
+          <FaTwitter size={72}/>
         </a>
         <a href="https://www.youtube.com/user/yfrhuang">
-          <FaYoutube size={72} />
+          <FaYoutube size={72}/>
         </a>
         <a href="https://t.me/yfr984">
-          <FaTelegram size={72} />
+          <FaTelegram size={72}/>
         </a>
       </section>
     </article>
-  );
+  )
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import Footer from './Footer'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM
+  .createRoot(document.getElementById('root') as HTMLElement)
+  .render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  )
+
+ReactDOM
+  .createRoot(document.getElementById('footer') as HTMLElement)
+  .render(
+    <React.StrictMode>
+      <Footer/>
+    </React.StrictMode>
+  )
