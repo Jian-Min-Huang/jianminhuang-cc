5c4e6a344e3e44b2b28907e813463252242c7222
Vincent Huang MacBook <yfr.huang@hotmail.com>
2024-01-07 18:56:40 +0800

    :sparkles: upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,126 @@
 import React from 'react';
 import ImageAvatar from './assets/avatar.webp';
-import { FaFacebook, FaGithub, FaGoogle, FaLinkedin, FaMicrophone, FaRegNewspaper, FaTelegram, FaTwitter, FaYoutube } from 'react-icons/fa6';
+import {
+  FaBuilding,
+  FaClock,
+  FaEnvelope,
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaLinkedin,
+  FaLocationDot,
+  FaMicrophoneLines,
+  FaTwitter,
+  FaYoutube
+} from 'react-icons/fa6';
 
 export default function App() {
+  const date: Date = new Date();
+
   return (
-    <article className={`my-8 flex flex-col items-center gap-8`}>
-      <img src={ImageAvatar} alt="Avatar" className={`h-64 w-64 rounded-full border-4 border-[#e89619] lg:h-80 lg:w-80`} />
-      <div className={`flex flex-col items-center gap-4 font-mono text-white`}>
-        <h1 className={`text-xl font-bold lg:text-2xl`}>Jian-Min (Vincent) Huang</h1>
-        <p className={`text-lg font-semibold lg:text-xl`}>
+    <article className={`mx-auto my-8 flex flex-col items-center gap-4 bg-gray-100`}>
+      <img src={ImageAvatar} alt="Avatar" className={`mx-auto h-64 w-64 rounded-full border-4 border-[#e89619]`} />
+      <h1 className={`mx-auto font-mono text-xl font-bold text-[#00386b]`}>Jian-Min (Vincent) Huang</h1>
+      <div className={`flex flex-col gap-1 font-mono text-[#00386b]`}>
+        <p className={`text-lg font-semibold`}>
           <span className={`text-[#e89619]`}>$_purefunc</span>, Co-founder
         </p>
-        <p className={`text-base font-medium lg:text-lg`}>👨‍💻Coder, 🎤Speaker, 👨‍🏫Mentor, 🚀Leader</p>
       </div>
-      <section className={`flex max-w-[360px] flex-wrap justify-center gap-8 text-white lg:max-w-[548px]`}>
-        <a href={`https://docs.google.com/document/d/1w3OKshhacU1vv2I6tAx-GSqNue8Nj_jm-_zHPkTzgrw`}>
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaRegNewspaper size={72} />CV
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaRegNewspaper size={84} />CV
-          </span>
-        </a>
-        <a href="https://www.facebook.com/yfr.huang/">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaFacebook size={72} />Facebook
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaFacebook size={84} />Facebook
-          </span>
-        </a>
-        <a href="https://github.com/Jian-Min-Huang/tech-note/wiki">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaMicrophone size={72} />Speech
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaMicrophone size={84} />Speech
-          </span>
-        </a>
-        <a href="https://developers.google.com/community/experts/directory/profile/profile-jian-min-huang">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaGoogle size={72} />Google
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaGoogle size={84} />Google
-          </span>
-        </a>
-        <a href="https://github.com/Jian-Min-Huang">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaGithub size={72} />GitHub
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaGithub size={84} />GitHub
-          </span>
-        </a>
-        <a href="https://www.linkedin.com/in/jian-min-huang">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaLinkedin size={72} />LinkedIn
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaLinkedin size={84} />LinkedIn
-          </span>
-        </a>
-        <a href="https://twitter.com/JianMin_V_Huang">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaTwitter size={72} />Twitter
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaTwitter size={84} />Twitter
-          </span>
-        </a>
-        <a href="https://www.youtube.com/user/yfrhuang">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaYoutube size={72} />YouTube
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaYoutube size={84} />YouTube
-          </span>
-        </a>
-        <a href="https://t.me/yfr984">
-          <span className={`flex flex-col items-center font-mono text-sm lg:hidden`}>
-            <FaTelegram size={72} />Telegram
-          </span>
-          <span className={`hidden items-center font-mono lg:flex lg:flex-col`}>
-            <FaTelegram size={84} />Telegram
-          </span>
-        </a>
+      <section className={`flex w-64 flex-col justify-start gap-2 font-mono font-medium text-base text-[#00386b]`}>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaBuilding size={20} />
+          <a
+            href={`https://purefunc.net`}
+            target="_blank"
+            rel="noreferrer">
+            PureFunc Inc.
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaLocationDot size={20} />
+          <a
+            href={`https://en.wikipedia.org/wiki/Taipei`}
+            target="_blank"
+            rel="noreferrer">
+            Taipei, Taiwan
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaClock size={20} />
+          <span className={`font-mono text-base`}>{`${date.getHours()}:${date.getMinutes()}`} (UTC +08:00)</span>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaEnvelope size={20} />
+          <a
+            href={`mailto:yfr.huang@hotmail.com`}
+            target="_blank"
+            rel="noreferrer">
+            yfr.huang@hotmail.com
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaLinkedin size={20} />
+          <a
+            href={`https://www.linkedin.com/in/jian-min-huang`}
+            target="_blank"
+            rel="noreferrer">
+            in/jian-min-huang
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaGithub size={20} />
+          <a
+            href={`https://github.com/Jian-Min-Huang`}
+            target="_blank"
+            rel="noreferrer">
+            @Jian-Min-Huang
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaGoogle size={20} />
+          <a
+            href={`https://g.dev/Vincent`}
+            target="_blank"
+            rel="noreferrer">
+            g.dev/Vincent
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaMicrophoneLines size={20} />
+          <a
+            href={`https://github.com/Jian-Min-Huang/tech-note/wiki`}
+            target="_blank"
+            rel="noreferrer">
+            Public Speaking
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaFacebook size={20} />
+          <a
+            href={`https://www.facebook.com/yfr.huang/`}
+            target="_blank"
+            rel="noreferrer">
+            黃健旻
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaTwitter size={20} />
+          <a
+            href={`"https://twitter.com/JianMin_V_Huang`}
+            target="_blank"
+            rel="noreferrer">
+            @JianMin_V_Huang
+          </a>
+        </div>
+        <div className={`flex flex-row items-center gap-2`}>
+          <FaYoutube size={20} />
+          <a
+            href={`https://www.youtube.com/user/yfrhuang`}
+            target="_blank"
+            rel="noreferrer">
+            @yfrhuang
+          </a>
+        </div>
       </section>
     </article>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import Footer from './Footer'
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <React.StrictMode>
       <App/>
-    </React.StrictMode>
-  )
-
-ReactDOM
-  .createRoot(document.getElementById('footer') as HTMLElement)
-  .render(
-    <React.StrictMode>
-      <Footer/>
     </React.StrictMode>
   )
